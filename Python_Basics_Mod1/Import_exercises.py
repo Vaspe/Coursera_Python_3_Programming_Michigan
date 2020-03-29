dd753fb88ebb0b41564414a33dc710e40861a0d5
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-03-29 18:21:13 +0200

    Create Import_exercises.py

---
+++ Python_Basics_Mod1/Import_exercises.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 16:55:38 2020
+
+@author: Vasilis
+"""
+
+import random
+
+#%% print 10 random numbers
+for i in range(10):
+    print(random.random())
+    
+#%% print 10 random numbers in range 25 35
+for i in range(10):
+    print(random.randrange(25,36))   
+    
+    
