72b9a91ce7311cfae7f09b1a16744e254a765abe
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-03 00:47:38 +0200

    Update Mutability_ch9.py

--- Python_Basics_Mod1/Mutability_ch9.py
+++ Python_Basics_Mod1/Mutability_ch9.py
@@ -174,3 +174,29 @@ print(setStr)
 
 
 print("-------------------------------")
+
+#%%
+#Append vs Concatenate
+
+origlist = [45,32,88]
+print("origlist:", origlist)
+print("the identifier:", id(origlist))             #id of the list before changes
+newlist = origlist + ['cat']
+print("newlist:", newlist)
+print("the identifier:", id(newlist))              #id of the list after concatentation
+origlist.append('cat')
+print("origlist:", origlist)
+print("the identifier:", id(origlist))             #id of the list after append is used
+print("-------------------------------")
+
+origlist = [45,32,88]
+print("origlist:", origlist)
+aliaslist = origlist
+print("aliaslist:", aliaslist)
+origlist += ["cat"]
+print(origlist)
+print (aliaslist)
+origlist = origlist + ["cow"]
+print("origlist:", origlist)
+print("aliaslist:", aliaslist)
+print("-------------------------------")
