9702068cefe82d2b685edd0d98780c46a4a7ac76
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-04 00:21:26 +0200

    Update Files_ch10.py

--- Python_Functions_Files_and_Dictionaries_Mod2/Files_ch10.py
+++ Python_Functions_Files_and_Dictionaries_Mod2/Files_ch10.py
@@ -3,5 +3,28 @@
 Created on Fri Apr  3 23:50:48 2020
 
 @author: Vasilis
+Useful refeerence:  https://www.w3schools.com/python/python_file_handling.asp
 """
+
+
+
+#%%
+
+
+fileref = open("olympics.txt", "r")
+## other code here that refers to variable fileref
+fileref.close()
+
+
+#%%
+
+
+
+
+
+#%%
+
+
+#%%
+
 
