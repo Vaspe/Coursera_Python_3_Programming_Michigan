5469f9ea2dc429818145307802199ad75ccd8cab
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-04 17:49:29 +0200

    Update all files

--- Python_Functions_Files_and_Dictionaries_Mod2/Files_ch10.py
+++ Python_Functions_Files_and_Dictionaries_Mod2/Files_ch10.py
@@ -73,4 +73,115 @@ fid = open ("Files/first_forty.txt","w")
 fid.write(input_str)
 fid.close
 
+#%%
+olympicsfile = open("Files/olympics.txt", "r")
 
+for aline in olympicsfile.readlines():
+    values = aline.split(",")
+    print(values[0], "is from", values[3], "and is on the roster for", values[4])
+
+olympicsfile.close()
+
+print ("----------------------------------------------------------")
+#%%
+olympicsfile = open("Files/olympics.txt", "r")
+ # we cana avoid readlines as python does it aurtomatically in loops. This is faste thanmreadlines
+for aline in olympicsfile:
+    values = aline.split(",")
+    print(values[0], "is from", values[3], "and is on the roster for", values[4])
+
+olympicsfile.close()
+
+#%% Using with for files
+with open('Files/mydata.txt', 'r') as md:
+    for line in md:
+        print(line)
+# with automates the close of the file since md is a file object and makes sure we clsose the file at the end
+
+#%%
+fid = open ("Files/squared_numbers.txt",'w')
+
+for i in range(13):
+    square = i*i
+    fid.write(str(square)+"\n")
+
+fid.close()
+
+print ("----------------------------------------------------------")
+#%% CSV files
+fid = open("Files/olympics.txt", "r")
+lines = fid.readlines()
+header = lines[0]
+field_names = header.strip().split(',') # we need to remove also white spaces around the variables!!!
+print("The field names are: " + str(field_names))
+for row in lines[1:]:
+    vals = row.strip().split(',')
+    if vals[5] != "NA": # only show athletes with medals
+        print("{}: {}; {}".format(vals[0],vals[4],vals[5]))
+fid.close()
+print ("----------------------------------------------------------")
+#%%
+fid = open("Files/SP500.txt")
+
+lines = fid.readlines()
+fid.close()
+header = lines[0]
+labels_nam = header.split(",")
+ind1 = labels_nam.index("SP500")
+ind2 = labels_nam.index("Long Interest Rate")
+
+cnt = 0 
+cnt2 = 0 
+SP_val_tot = 0
+max_interest = 0
+SP_val = []
+interest_val = []
+
+for i in lines[1:]:
+    curline = i
+    curline = curline.split(",")
+    SP_val = SP_val + [curline[ind1]]
+    interest_val = interest_val+ [float(curline[ind2])]
+    if cnt >=5 and cnt<=16:
+        SP_val_tot =  SP_val_tot + float(SP_val[cnt])
+        if max_interest< float(interest_val[cnt]): 
+            max_interest = float(interest_val[cnt])
+        cnt2 = cnt2 +1
+    cnt =cnt+1;
+    
+mean_SP = SP_val_tot/cnt2
+
+
+#%% Writing a CSV
+olympians = [("John Aalberg", 31, "Cross Country Skiing"),
+             ("Minna Maarit Aalto", 30, "Sailing"),
+             ("Win Valdemar Aaltonen", 54, "Art Competitions"),
+             ("Wakako Abe", 18, "Cycling")]
+
+outfile = open("Files/reduced_olympics.csv", "w")
+# output the header row
+outfile.write('Name,Age,Sport')
+outfile.write('\n')
+# output each of the rows:
+for olympian in olympians:
+    row_string = '{},{},{}'.format(olympian[0], olympian[1], olympian[2])
+    outfile.write(row_string)
+    outfile.write('\n')
+outfile.close()    
+        
+#%%
+olympians = [("John Aalberg", 31, "Cross Country Skiing, 15KM"),
+             ("Minna Maarit Aalto", 30, "Sailing"),
+             ("Win Valdemar Aaltonen", 54, "Art Competitions"),
+             ("Wakako Abe", 18, "Cycling")]
+
+outfile = open("Files/reduced_olympics2.csv", "w")
+# output the header row
+outfile.write('"Name","Age","Sport"')
+outfile.write('\n')
+# output each of the rows:
+for olympian in olympians:
+    row_string = '"{}", "{}", "{}"'.format(olympian[0], olympian[1], olympian[2])
+    outfile.write(row_string)
+    outfile.write('\n')
+outfile.close()
