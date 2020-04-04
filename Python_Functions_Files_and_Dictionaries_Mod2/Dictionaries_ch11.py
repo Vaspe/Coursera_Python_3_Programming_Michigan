f02ab7db1dd719525c5d306fb50f8c240e02b668
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-04 20:17:38 +0200

    Update Dictionaries_ch11.py

--- Python_Functions_Files_and_Dictionaries_Mod2/Dictionaries_ch11.py
+++ Python_Functions_Files_and_Dictionaries_Mod2/Dictionaries_ch11.py
@@ -5,7 +5,22 @@ Created on Sat Apr  4 19:12:12 2020
 @author: Vasilis
 Nice overview of the dictionaries and their methods:
     https://www.w3schools.com/python/python_dictionaries.asp
+    https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
 
+
+Method	Description
+
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get() 	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	      Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
 """
 #%% creating a dictionary, starting from an emoty one
 eng2sp = {}
@@ -38,10 +53,17 @@ print("-------------------------------------------------------")
 inventory = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
 print (inventory)
 del inventory['pears']
-print(inventory)
+print("Using the command del as del inventory['pears'], gives:  " +str(inventory))
+len(inventory)
+print("Using the command len gives the amount of key-value pairs in a dictionary, len(inventpry):  " +str(len(inventory)))
 
+print("-------------------------------------------------------")
+#%% main methods
 
+print("---------------------MAIN METHODS----------------------------------")
 
+inventory = {'apples': 430, 'bananas': 312, 'oranges': 525, 'pears': 217}
 
+key_list = list(inventory.keys()) # the method its self does not produce a list!!!!!
 
 
