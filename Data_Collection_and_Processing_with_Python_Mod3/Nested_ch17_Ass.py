9083afa81b38591c4de2f03d769ae0ebac461d2a
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-10 16:09:28 +0200

    Update Nested_ch17_Ass.py

--- Data_Collection_and_Processing_with_Python_Mod3/Nested_ch17_Ass.py
+++ Data_Collection_and_Processing_with_Python_Mod3/Nested_ch17_Ass.py
@@ -5,3 +5,93 @@ Created on Fri Apr 10 15:44:54 2020
 @author: Vasilis
 """
 
+#%% Ass1
+nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
+
+output = nested[1][2]
+
+#%%Ass2
+lst = [['apple', 'orange', 'banana'], [5, 6, 7, 8, 9.9, 10], ['green', 'yellow', 'purple', 'red']]
+
+#Test to see if 'yellow' is in the third list of lst. Save to variable ``yellow``
+yellow = 'yellow' in lst[2]
+
+#Test to see if 4 is in the second list of lst. Save to variable ``four``
+four = 4 in lst[1]
+
+#Test to see if 'orange' is in the first element of lst. Save to variable ``orange``
+orange = 'orange' in lst[0]
+
+
+#%%Ass3
+L = [[5, 8, 7], ['hello', 'hi', 'hola'], [6.6, 1.54, 3.99], ['small', 'large']]
+
+# Test if 'hola' is in the list L. Save to variable name test1
+test1 = 'hola' in L
+# Test if [5, 8, 7] is in the list L. Save to variable name test2
+test2 = [5,8,7] in L
+# Test if 6.6 is in the third element of list L. Save to variable name test3
+test3 = 6.6 in L[2]
+
+
+#%%Ass4
+nested = {'data': ['finding', 23, ['exercises', 'hangout', 34]], 'window': ['part', 'whole', [], 'sum', ['math', 'calculus', 'algebra', 'geometry', 'statistics',['physics', 'chemistry', 'biology']]]}
+
+# Check to see if the string data is a key in nested, if it is, assign True to the variable data, otherwise assign False.
+data = 'data' in nested
+# Check to see if the integer 24 is in the value of the key data, if it is then assign to the variable twentyfour the value of True, otherwise False.
+twentyfour = 24 in nested['data'] 
+# Check to see that the string 'whole' is not in the value of the key window. If it's not, then assign to the variable whole the value of True, otherwise False.
+whole = 'whole' not in nested['window']
+# Check to see if the string 'physics' is a key in the dictionary nested. If it is, assign to the variable physics, the value of True, otherwise False.
+physics = 'physics' in nested
+
+
+#%%Ass5
+nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
+
+london_gold = nested_d['London']['Great Britain']
+
+#%%Ass6
+sports = {'swimming': ['butterfly', 'breaststroke', 'backstroke', 'freestyle'], 'diving': ['springboard', 'platform', 'synchronized'], 'track': ['sprint', 'distance', 'jumps', 'throws'], 'gymnastics': {'women':['vault', 'floor', 'uneven bars', 'balance beam'], 'men': ['vault', 'parallel bars', 'floor', 'rings']}}
+
+# Assign the string 'backstroke' to the name v1
+v1 = sports['swimming'][2]
+# Assign the string 'platform' to the name v2
+v2 = sports['diving'][1]
+# Assign the list ['vault', 'floor', 'uneven bars', 'balance beam'] to the name v3
+v3 = sports['gymnastics']['women']
+# Assign the string 'rings' to the name v4
+v4 = sports['gymnastics']['men'][3]
+print(v4)
+
+#%% Ass7
+nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
+
+US_count = []
+for i in nested_d:
+    US_count = US_count + [nested_d[i]['USA']]
+
+#%% Ass 8
+l_of_l = [['purple', 'mauve', 'blue'], ['red', 'maroon', 'blood orange', 'crimson'], ['sea green', 'cornflower', 'lavender', 'indigo'], ['yellow', 'amarillo', 'mac n cheese', 'golden rod']]
+third = []
+for i1 in l_of_l:
+    third.append(i1[2])
+
+
+#%% Ass 9
+athletes = [['Phelps', 'Lochte', 'Schooling', 'Ledecky', 'Franklin'], ['Felix', 'Bolt', 'Gardner', 'Eaton'], ['Biles', 'Douglas', 'Hamm', 'Raisman', 'Mikulak', 'Dalton']]
+t= []
+other = []
+for i1 in athletes:
+    for i2 in i1:
+        if 't' in i2:
+            t = t + [i2]
+        else:
+            other = other + [i2]
+
+
+
+
+
+
