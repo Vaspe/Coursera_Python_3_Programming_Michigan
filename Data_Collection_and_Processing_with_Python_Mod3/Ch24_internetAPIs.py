fd3230be9d30bdf35e8df68c45ba80f60427ac5b
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-13 23:55:23 +0200

    Create Ch24_internetAPIs.py

---
+++ Data_Collection_and_Processing_with_Python_Mod3/Ch24_internetAPIs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 23:44:06 2020
+
+@author: Vasilis
+
+look at this guy: 
+    https://en.wikipedia.org/wiki/John_Gilmore_(activist)
+        https://en.wikipedia.org/wiki/Drug_policy_reform
+
+        
+    
+"""
+
