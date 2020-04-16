b503e7404d3d21de110d14d0076f84f27b805812
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-17 00:44:05 +0200

    Create Ch20_classes_Ass.py

---
+++ Python_Classes_and_Inheritance_Mod4/Ch20_classes_Ass.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 00:25:14 2020
+
+@author: Vasilis
+"""
+
+
+#%% Ass 1
+class Bike:
+    def __init__ (self,color,price):
+        self.color = color
+        self.price = price
+        
+testOne = Bike('blue',89.99)        
+testTwo = Bike('purple', 25.0)
+print(testOne.color)
+
+#%% Ass2
+
+class AppleBasket:
+    def __init__ (self,apple_color,apple_quantity):
+        self.apple_color = apple_color
+        self.apple_quantity = apple_quantity
+        
+    def increase(self):
+        self.apple_quantity = self.apple_quantity + 1
+     
+    def __str__(self):
+        return "A basket of {} {} apples.".format(self.apple_quantity,self.apple_color)
+        
+TestOne = AppleBasket('red',4)
+print(TestOne)
+   
+
+
+
+
+#%%
