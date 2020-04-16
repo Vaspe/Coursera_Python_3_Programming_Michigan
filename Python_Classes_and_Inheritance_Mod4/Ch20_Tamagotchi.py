2a3f3433b27b15c7bc6f8d4ccc050ca8762f6823
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-16 21:59:47 +0200

    Update Ch20_Tamagotchi.py

--- Python_Classes_and_Inheritance_Mod4/Ch20_Tamagotchi.py
+++ Python_Classes_and_Inheritance_Mod4/Ch20_Tamagotchi.py
@@ -8,4 +8,15 @@ import random
 import copy
 
 
-def
+class Pet:
+    boredom_decrement = 4
+    hunger_decrement = 6
+    boredom_threshold = 5
+    hunger_threshold = 10
+    sounds = ['Mrrp', 'Aaarghh', 'Miauuuuu'] 
+    
+    def __init__ (self, name = 'Kitty',init_hunger = random.randrange(hunger_threshold) , init_boredom =random.randrange(boredom_threshold),init_sounds = copy.deepcopy(sounds)):
+        self.name = name
+        self.hunger = init_hunger
+        self.boredom = randrange(self.boredom_threshold)
+        self.sounds = init_sounds 
