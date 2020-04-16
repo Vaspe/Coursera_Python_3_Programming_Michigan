de009118aec14e2b00ce46ad0904ec8fb84b3559
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-04-16 22:13:07 +0200

    Update Ch20_Tamagotchi.py

--- Python_Classes_and_Inheritance_Mod4/Ch20_Tamagotchi.py
+++ Python_Classes_and_Inheritance_Mod4/Ch20_Tamagotchi.py
@@ -15,8 +15,43 @@ class Pet:
     hunger_threshold = 10
     sounds = ['Mrrp', 'Aaarghh', 'Miauuuuu'] 
     
-    def __init__ (self, name = 'Kitty',init_hunger = random.randrange(hunger_threshold) , init_boredom =random.randrange(boredom_threshold),init_sounds = copy.deepcopy(sounds)):
+    def __init__ (self, name = 'Kitty',init_hunger = random.randrange(0,hunger_threshold,1) , init_boredom =random.randrange(0,boredom_threshold,1),init_sounds = copy.deepcopy(sounds)):
         self.name = name
         self.hunger = init_hunger
         self.boredom = init_boredom
         self.sounds = init_sounds 
+        
+    def mood(self):
+        if self.hunger <= self.hunger_threshold and self.boredom <= self.boredom_threshold:
+            return "happy"
+        elif self.hunger > self.hunger_threshold:
+            return "hungry"
+        else:
+            return "bored"
+
+    def clock_tick(self):
+        self.boredom = self.boredom + 1
+        self.hunger =self.hunger + 1
+
+    def hi(self):
+        self.reduce_boredom()
+        print(sounds[random.randrange(len(sounds))])
+     
+    def teach(self,word):
+        self.sounds.append(word)
+        self.reduce_boredom
+        
+    def feed(self):
+        self.reduce_hunger()
+    
+    def reduce_hunger(self):
+        self.hunger = max(0, self.hunger - self.hunger_decrement)
+
+    def reduce_boredom(self):
+        self.boredom = max(0, self.boredom - self.boredom_decrement)
+    
+    def __str__(self):    
+        state = "I'm {}. \nI feel {}.\n".format(self.name,self.mood())
+        state = state + "Hunger {} Boredom {} Words {}".format(self.hunger, self.boredom, self.sounds)
+        return state
+                
