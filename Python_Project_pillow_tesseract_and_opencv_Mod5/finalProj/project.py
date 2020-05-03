6722d06c87bd8c018fd2b5b3854bba6c913c70ed
Vaspe <32874312+Vaspe@users.noreply.github.com>
2020-05-03 18:46:51 +0200

    u[date

--- Python_Project_pillow_tesseract_and_opencv_Mod5/finalProj/project.py
+++ Python_Project_pillow_tesseract_and_opencv_Mod5/finalProj/project.py
@@ -40,48 +40,93 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 #Define the zip file name with the pngs
 zip_name = "D:/Github/small_img.zip"
 # zip_name = "D:/Github/images.zip"
-# loading the face detection classifier
-face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
 
-# Creating zipfile object 
-myzip = zipfile.ZipFile(zip_name, 'r')
-
-#take the info from thefile
-info_list = myzip.infolist() # not useful...
-# print(info_list)
-
-#Taking all the file names in the zip 
-fl_names = myzip.namelist()
-
-#Creating a dictionary wtih keys the image names and values a li st with first element the image object itself
-img_dict = {}
-images_arr = []
-size = 128, 128
-for i in fl_names:
-    img_dict[i] = [Image.open(BytesIO(myzip.read(i)))]
-    img_dict[i] = img_dict[i] + [np.asarray(img_dict[i][0])]
-    #display(img_dict[i][0])    
+def get_imdict(zip_name):
+    # loading the face detection classifier
+    face_cascade = cv.CascadeClassifier('readonly/haarcascade_frontalface_default.xml')
     
-    # get the text of each page
-    img_dict[i] = img_dict[i] + [pytesseract.image_to_string(img_dict[i][0])]
+    # Creating zipfile object 
+    myzip = zipfile.ZipFile(zip_name, 'r')
     
-    #find the faces,crop them,  and store them in a list
-    cv_im = cv.cvtColor(img_dict[i][1],cv.COLOR_BGR2HSV)
-    faces = face_cascade.detectMultiScale(cv_im,1.2,4)
-    small_faces = []
-    pil_img = img_dict[i][0]
-    for x,y,w,h in faces:
-        cur_im = pil_img.copy()
-        #crop the face
-        aa = cur_im.crop((x,y,x+w,y+h))
-        # resize cropped to 128x128
-        aa.thumbnail(size)
-        # display(aa)
-        #add to the list of faces
-        small_faces = small_faces + [aa]
-    img_dict[i] =img_dict[i] + [small_faces]
-myzip.close()
+    #take the info from thefile
+    info_list = myzip.infolist() # not useful...
+    # print(info_list)
+    
+    #Taking all the file names in the zip 
+    fl_names = myzip.namelist()
+    
+    #Creating a dictionary wtih keys the image names and values a li st with first element the image object itself
+    img_dict = {}
+    images_arr = []
+    size = 128, 128
+    for i in fl_names:
+        img_dict[i] = [Image.open(BytesIO(myzip.read(i)))]
+        img_dict[i] = img_dict[i] + [np.asarray(img_dict[i][0])]
+        #display(img_dict[i][0])    
+        
+        # get the text of each page
+        img_dict[i] = img_dict[i] + [pytesseract.image_to_string(img_dict[i][0])]
+        
+        #find the faces,crop them,  and store them in a list
+        cv_im = cv.cvtColor(img_dict[i][1],cv.COLOR_BGR2HSV)
+        faces = face_cascade.detectMultiScale(cv_im,1.2,4)
+        small_faces = []
+        pil_img = img_dict[i][0]
+        for x,y,w,h in faces:
+            cur_im = pil_img.copy()
+            #crop the face
+            aa = cur_im.crop((x,y,x+w,y+h))
+            # resize cropped to 128x128
+            aa.thumbnail(size)
+            # display(aa)
+            #add to the list of faces
+            small_faces = small_faces + [aa]
+        img_dict[i] =img_dict[i] + [small_faces]
+    myzip.close()
+    
+
+    return img_dict     
+
+#%% logic:
+
+img_dict = get_imdict(zip_name)
+wrd = 'Christopher'
+
+# create a contact sheet from different brightnesses
+first_image=images[0]
+contact_sheet=PIL.Image.new(first_image.mode, (first_image.width*3,first_image.height*3))
+x=0
+y=0
+
+for img in images:
+    # Lets paste the current image into the contact sheet
+    contact_sheet.paste(img, (x, y) )
+    # Now we update our X position. If it is going to be the width of the image, then we set it to 0
+    # and update Y as well to point to the next "line" of the contact sheet.
+    if y+first_image.height == contact_sheet.height:
+        x=x+first_image.width
+        y=0
+    else:
+        y=y+first_image.height        
+
 
 # for i in img_dict['a-0.png'][3]:
 #     display(i)
+
+for i in  img_dict:
+    # check for word in the text
+    if wrd in img_dict[i][2]:
+        print('Results found in ' + i)
+        if img_dict[i][3]:
+            for ii in img_dict['a-0.png'][3]:
+                display(ii)
+        else:
+            print('But there were no faces in that file')        
+    else:
+        print('No results found in ' + i)
+        continue
     
+    
+
+
+
